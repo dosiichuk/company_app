7bffdcff884a98107c45d41fe5357547b477964f
Dmytro <dmytro.osiychuk@gmail.com>
2022-04-01 15:21:54 +0200

    Add tests to Employee and Department schemas

---
+++ models/test/department.test.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Department = require('../department.model.js');
+const expect = require('chai').expect;
+
+describe('Department', () => {
+  it('should throw an error if no "name" arg', () => {
+    const dep = new Department({}); // create new Department, but don't set `name` attr value
+
+    dep.validate((err) => {
+      expect(err.errors.name).to.exist;
+    });
+  });
+  it('should throw an error if name is longer than 20 and shorter than 5 chars', () => {
+    const cases = ['jkfn', 'jdsfieajrfieurifeurfiueirufherfheoihf'];
+    for (let name of cases) {
+      const dep = new Department({ name });
+      dep.validate((err) => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+  });
+  it('should throw an error if "name" is not a string', () => {
+    const cases = [{}, []];
+    for (let name of cases) {
+      const dep = new Department({ name });
+
+      dep.validate((err) => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+  });
+  it('should not throw error if name is consistent with schema', () => {
+    const cases = ['jkfnff', 'rieurhifu78'];
+    for (let name of cases) {
+      const dep = new Department({ name });
+      dep.validate((err) => {
+        expect(err).to.equal(null);
+      });
+    }
+  });
+});
+after(() => {
+  mongoose.models = {};
+});
---
+++ models/test/employee.test.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const expect = require('chai').expect;
+const Employee = require('../employee.model');
+
+describe('Employee', () => {
+  it('should throw an error if no firstName, lastName or department arg', () => {
+    const cases = [
+      { lastName: 'Doe', department: 'Management' },
+      { firstName: 'Doe', department: 'Management' },
+      { lastName: 'Doe', firstName: 'Management' },
+      {},
+      { firstName: 'John' },
+    ];
+    for (let item of cases) {
+      const employee = new Employee({ ...item });
+      employee.validate((err) => {
+        expect(err.errors).to.exist;
+      });
+    }
+  });
+  it('should throw an error if firstName, lastName or department is not a string', () => {
+    const cases = [
+      { lastName: [], department: 'Management' },
+      { firstName: 'Doe', department: {} },
+      { lastName: 'Doe', firstName: () => {} },
+      {},
+      { firstName: null },
+    ];
+    for (let item of cases) {
+      const employee = new Employee({ ...item });
+
+      employee.validate((err) => {
+        expect(err.errors).to.exist;
+      });
+    }
+  });
+  it('should not throw error if entry is consistent with schema', () => {
+    const cases = [
+      { firstName: 'John', lastName: 'Doe', department: 'Management' },
+      { firstName: 'Amanda', lastName: 'Doe', department: 'Fconomy' },
+    ];
+    for (let item of cases) {
+      const employee = new Employee(item);
+      employee.validate((err) => {
+        expect(err).to.not.exist;
+      });
+    }
+  });
+});
+
+after(() => {
+  mongoose.models = {};
+});
